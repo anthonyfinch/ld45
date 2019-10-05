0e50337f401289671ce66b26934918602c8b4b9a
Anthony Finch <actionspeakslouder@gmail.com>
2019-10-05 18:27:06 +0100

    sorts out player jumping

--- player.gd
+++ player.gd
@@ -11,7 +11,6 @@ var floor_normal = Vector2(0, -1)
 
 func _input(event):
 	if event.is_action_pressed("jump"):
-		print("jumped")
 		if is_on_floor():
 			y_vel = jump_speed
 
@@ -24,19 +23,17 @@ func _physics_process(delta):
 		movement.x = move_speed
 
 	if !is_on_floor():
-		print("not on floor")
 		if y_vel > 0:
 			if is_on_ceiling():
 				y_vel = 0
 			y_vel = y_vel - (jump_gravity * delta)
-			print("jumpfall")
 		else:
 			y_vel = y_vel - (fall_gravity * delta)
-			print("FAALING")
-		movement.y = -1 * y_vel
 
 	else:
-		print("on floor")
-		movement.y = -1 * y_vel
+		if y_vel <= 0:
+			y_vel = -10
+
+	movement.y = -1 * y_vel
 
 	move_and_slide(movement, floor_normal)
